4e6541c38645ddd6174addb0ce56af045ffa3afa
pausim3 <Paulius5432109@gmail.com>
2021-10-08 10:37:29 +0300

    Strategy

---
+++ ProjectClient/Class/Strategy/Algorithm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectClient.Class.Strategy
+{
+    abstract class Algorithm
+    {
+        public abstract void ItemActivated();
+    }
+}
---
+++ ProjectClient/Class/Strategy/EnemyAlgorithm .cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectClient.Class.Strategy
+{
+    class EnemyAlgorithm : Algorithm
+    {
+        public override void ItemActivated()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
