482ab4ea668d364d054859999ccba53b771974fc
pausim3 <Paulius5432109@gmail.com>
2021-10-15 00:43:10 +0300

    Added more customization

--- ProjectClient/GameMap.cs
+++ ProjectClient/GameMap.cs
@@ -22,6 +22,7 @@ namespace ProjectClient
 
         private Graphics g;
         private const int BLOCK_SIZE = 45;
+        private List<string> groupPlayers;
         GraphicalElement player1;
         GraphicalElement player2;
 
@@ -39,6 +40,7 @@ namespace ProjectClient
         private string MapLayout;
         private int Level = 1;
 
+        private const int EMPTY_ID = 5;
         private const int SPACE_ID = 0;
         private const int WALL_ID = 1;
         private const int PLAYER_ID = 2;
@@ -52,6 +54,7 @@ namespace ProjectClient
         public static int MAP_MAX_SIZE = 20;
         public static int MAP_MIN_SIZE = 0;
         private int[,] MapMatrix = new int[21, 21];
+        private GraphicalElement[,] ObjectMatrix = new GraphicalElement[21, 21];
         private int[,] DefaultMap = new int[,] {
             { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
             { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
@@ -60,13 +63,13 @@ namespace ProjectClient
             { 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1 },
             { 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
             { 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1 },
-            { 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1 },
-            { 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 3, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1 },
+            { 1, 5, 5, 5, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 5, 5, 5, 1 },
+            { 1, 10, 20, 40, 1, 0, 1, 0, 0, 0, 3, 0, 0, 0, 1, 0, 1, 10, 20, 40, 1 },
             { 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1 },
-            { 1, 2, 0, 0, 0, 0, 0, 0, 1, 10, 20, 40, 1, 0, 0, 0, 0, 0, 0, 2, 1 },
+            { 1, 2, 0, 0, 0, 0, 0, 0, 1, 5, 5, 5, 1, 0, 0, 0, 0, 0, 0, 2, 1 },
             { 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1 },
-            { 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1 },
-            { 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1 },
+            { 1, 5, 5, 5, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 5, 5, 5, 1 },
+            { 1, 5, 5, 5, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 5, 5, 5, 1 },
             { 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1 },
             { 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
             { 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1 },
@@ -81,6 +84,7 @@ namespace ProjectClient
             this.connection = connection;
             this.connectionId = connectionId;
             this.groupName = groupName;
+            this.groupPlayers = groupPlayers;
 
             /*this.connection = new HubConnectionBuilder()
                 .WithUrl("http://localhost:5000/gamehub")
@@ -120,6 +124,8 @@ namespace ProjectClient
                 txtPlayer2Id.Text = "Error: incorrect group size";
             }
 
+            CreateObjectMatrix();
+
             //Singleton Inner class approach
             GameRound gameRound = GameRound.getInstance();
 
@@ -143,7 +149,7 @@ namespace ProjectClient
                 case 1:
                     player1.X = 19;
                     player1.Y = 10;
-                    player2.X = 19;
+                    player2.X = 1;
                     player2.Y = 10;
                     break;
             }
@@ -159,8 +165,7 @@ namespace ProjectClient
 
             Item testCoin = new Coin();
             subject.Attach(testCoin);
-
-            testCoin.UseItem(); 
+            testCoin.UseItem();
 
             //Abstract Factory
             //randomizing special item level
@@ -201,9 +206,126 @@ namespace ProjectClient
             itemCoin.ItemActivated();
         }
 
+        private void CreateObjectMatrix()
+        {
+            //Factory
+            Creator creator = new GraphicalElementCreator();
+
+            player1 = creator.FactoryMethod(PLAYER_ID);
+            player2 = creator.FactoryMethod(PLAYER_ID);
+
+            switch (groupPlayers.IndexOf(connectionId))
+            {
+                case 0:
+                    player1.X = 1;
+                    player1.Y = 10;
+                    player2.X = 19;
+                    player2.Y = 10;
+
+                    ObjectMatrix[1, 10] = player1;
+                    ObjectMatrix[19, 10] = player2;
+                    break;
+                case 1:
+                    player1.X = 19;
+                    player1.Y = 10;
+                    player2.X = 1;
+                    player2.Y = 10;
+
+                    ObjectMatrix[19, 10] = player1;
+                    ObjectMatrix[1, 10] = player2;
+                    break;
+            }
+
+            for (int i = 0; i < 21; i++)
+            {
+                for (int j = 0; j < 21; j++)
+                {
+                    int blockId = MapMatrix[j, i];
+
+                    switch (blockId)
+                    {
+                        case EMPTY_ID:
+                            break;
+                        case SPACE_ID:
+                            break;
+                        case PLAYER_ID:
+                            break;
+                        case WALL_ID:
+                            GraphicalElement wall = creator.FactoryMethod(WALL_ID);
+                            ObjectMatrix[i, j] = wall;
+                            break;
+                        case COIN_ID:
+                            //GraphicalElement coin = creator.FactoryMethod(COIN_ID);
+                            //ObjectMatrix[i, j] = coin;
+                            break;
+                        case SPECIAL_WALL_ID:
+                            //GraphicalElement specwall = creator.FactoryMethod(COIN_ID);
+                            //ObjectMatrix[i, j] = specwall;
+                            break;
+                        case DESTROYER_ID:
+                            //GraphicalElement destroyer = creator.FactoryMethod(COIN_ID);
+                            //ObjectMatrix[i, j] = destroyer;
+                            break;
+                        case SPIKES_ID:
+                            //GraphicalElement spikes = creator.FactoryMethod(COIN_ID);
+                            //ObjectMatrix[i, j] = spikes;
+                            break;
+                    }
+                }
+            }
+        }
+
         private void SpawnItemToMap<T>(T item)
         {
             //spawn item
+            bool found = false;
+            int x, y;
+            int blockId;
+            Random rnd = new Random();
+
+            while (!found)
+            {
+                x = rnd.Next(MAP_MIN_SIZE + 1, MAP_MAX_SIZE - 1);
+                y = rnd.Next(MAP_MIN_SIZE + 1, MAP_MAX_SIZE - 1);
+                blockId = MapMatrix[x, y];
+                if (blockId == SPACE_ID)
+                {
+                    found = true;
+
+                    ObjectMatrix[x, y] = item as Item;
+
+                    if (item is DefaultSpecialWall)
+                    {
+                        MapMatrix[x, y] = SPECIAL_WALL_ID;
+                    }
+
+                    if (item is UpgradedSpecialWall)
+                    {
+                        MapMatrix[x, y] = SPECIAL_WALL_ID;
+                    }
+
+                    if (item is DefaultSpikes)
+                    {
+                        MapMatrix[x, y] = SPIKES_ID;
+                    }
+
+                    if (item is UpgradedSpikes)
+                    {
+                        MapMatrix[x, y] = SPIKES_ID;
+                    }
+
+                    if (item is Coin)
+                    {
+                        MapMatrix[x, y] = COIN_ID;
+                    }
+
+                    if (item is Destroyer)
+                    {
+                        MapMatrix[x, y] = DESTROYER_ID;
+                    }
+                }
+            }
+            Draw();
         }
 
         //dont need ?
@@ -225,39 +347,39 @@ namespace ProjectClient
                 Draw();
             }
         }
-        
+
         private void UpdatePlayerPosition(int pos)
         {
             //need to fix if's to not let player step on wall
             switch (pos)
             {
                 case LEFT:
-                    //if (player1.X - 1 > MAP_MIN_SIZE && (MapMatrix[player1.X - 1, player1.Y] != WALL_ID && MapMatrix[player1.X - 1, player1.Y] != PLAYER_ID))
-                    // {
-                    SetMap(player1.X, player1.Y, SPACE_ID);
-                    player1.X = player1.X - 1;
-                    // }
+                    if (player1.X - 1 > MAP_MIN_SIZE && (MapMatrix[player1.X - 1, player1.Y] != WALL_ID && MapMatrix[player1.X - 1, player1.Y] != PLAYER_ID))
+                    {
+                        SetMap(player1.X, player1.Y, SPACE_ID);
+                        player1.X = player1.X - 1;
+                    }
                     break;
                 case UP:
-                    //if (player1.Y - 1 > MAP_MIN_SIZE && (MapMatrix[player1.X, player1.Y - 1] != WALL_ID && MapMatrix[player1.X, player1.Y - 1] != PLAYER_ID)) 
-                    //{
-                    SetMap(player1.X, player1.Y, SPACE_ID);
-                    player1.Y = player1.Y - 1;
-                    // }
+                    if (player1.Y - 1 > MAP_MIN_SIZE && (MapMatrix[player1.X, player1.Y - 1] != WALL_ID && MapMatrix[player1.X, player1.Y - 1] != PLAYER_ID))
+                    {
+                        SetMap(player1.X, player1.Y, SPACE_ID);
+                        player1.Y = player1.Y - 1;
+                    }
                     break;
                 case RIGHT:
-                    // if (player1.X + 1 < MAP_MAX_SIZE && (MapMatrix[player1.X + 1, player1.Y] != WALL_ID && MapMatrix[player1.X + 1, player1.Y] != PLAYER_ID))
-                    // {
-                    SetMap(player1.X, player1.Y, SPACE_ID);
-                    player1.X = player1.X + 1;
-                    //  }
+                    if (player1.X + 1 < MAP_MAX_SIZE && (MapMatrix[player1.X + 1, player1.Y] != WALL_ID && MapMatrix[player1.X + 1, player1.Y] != PLAYER_ID))
+                    {
+                        SetMap(player1.X, player1.Y, SPACE_ID);
+                        player1.X = player1.X + 1;
+                    }
                     break;
                 case DOWN:
-                    //  if (player1.Y + 1 < MAP_MAX_SIZE && (MapMatrix[player1.X, player1.Y + 1] != WALL_ID && MapMatrix[player1.X, player1.Y + 1] != PLAYER_ID))
-                    //  {
-                    SetMap(player1.X, player1.Y, SPACE_ID);
-                    player1.Y = player1.Y + 1;
-                    //  }
+                    if (player1.Y + 1 < MAP_MAX_SIZE && (MapMatrix[player1.X, player1.Y + 1] != WALL_ID && MapMatrix[player1.X, player1.Y + 1] != PLAYER_ID))
+                    {
+                        SetMap(player1.X, player1.Y, SPACE_ID);
+                        player1.Y = player1.Y + 1;
+                    }
                     break;
             }
             connection.InvokeCoreAsync("UpdatePlayerPos", args: new[] { player1.X.ToString(), player1.Y.ToString(), connectionId, groupName });
@@ -290,13 +412,16 @@ namespace ProjectClient
                 for (int j = 0; j < 21; j++)
                 {
                     int blockId = MapMatrix[j, i];
-                    if (blockId != SPIKES_ID) {
+                    GraphicalElement element = ObjectMatrix[i, j];
+
+                    if (blockId != SPIKES_ID)
+                    {
                         rectangle = new Rectangle(i * BLOCK_SIZE, j * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
                     }
                     else
                     {
-                        PointF point1 = new PointF(i * BLOCK_SIZE, (j+1) * BLOCK_SIZE);
-                        PointF point2 = new PointF((i * BLOCK_SIZE)+(BLOCK_SIZE/2), (j * BLOCK_SIZE));
+                        PointF point1 = new PointF(i * BLOCK_SIZE, (j + 1) * BLOCK_SIZE);
+                        PointF point2 = new PointF((i * BLOCK_SIZE) + (BLOCK_SIZE / 2), (j * BLOCK_SIZE));
                         PointF point3 = new PointF((i * BLOCK_SIZE) + BLOCK_SIZE, (j + 1) * BLOCK_SIZE);
                         triangle[0] = point1;
                         triangle[1] = point2;
@@ -318,14 +443,36 @@ namespace ProjectClient
                             g.FillEllipse(Brushes.Yellow, rectangle);
                             break;
                         case SPECIAL_WALL_ID:
-                            g.FillRectangle(Brushes.AliceBlue, rectangle);
+                            if (element is DefaultSpecialWall)
+                            {
+                                g.FillRectangle(Brushes.DeepSkyBlue, rectangle);
+                            }
+                            else if (element is UpgradedSpecialWall)
+                            {
+                                g.FillRectangle(Brushes.DarkBlue, rectangle);
+                            }
+                            else
+                            {
+                                g.FillRectangle(Brushes.AliceBlue, rectangle);
+                            }
                             break;
                         case DESTROYER_ID:
                             //g.DrawRectangle(new Pen(Brushes.DarkMagenta, 3), rectangle);
                             g.FillRectangle(Brushes.DarkMagenta, rectangle);
                             break;
                         case SPIKES_ID:
-                            g.FillPolygon(Brushes.White, triangle);
+                            if (element is DefaultSpecialWall)
+                            {
+                                g.FillPolygon(Brushes.DeepSkyBlue, triangle);
+                            }
+                            else if (element is UpgradedSpecialWall)
+                            {
+                                g.FillPolygon(Brushes.DarkBlue, triangle);
+                            }
+                            else
+                            {
+                                g.FillPolygon(Brushes.White, triangle);
+                            }
                             break;
                     }
                 }
