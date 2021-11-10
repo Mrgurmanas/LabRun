ab4b136948c653ce83205c8db33fdcbc9a26f5fe
adomej99 <mejarasadomas@gmail.com>
2021-11-11 00:30:36 +0200

    builder

---
+++ ProjectClient/Class/Builder/Builder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProjectClient.Class.AbstractFactory;
+
+namespace ProjectClient.Class.Builder
+{
+    abstract class Builder
+    {
+        public Spikes spikes;
+        public SpecialWall specialWall;
+
+        public Builder startNewSpikes(Spikes newSpikes)
+        {
+            this.spikes = newSpikes;
+            return this;
+        }
+
+        public Builder startNewSpecialWall(SpecialWall newSpecialWall)
+        {
+            this.specialWall = newSpecialWall;
+            return this;
+        }
+
+        public Spikes getBuildableSpikes()
+        {
+            return this.spikes;
+        }
+        public SpecialWall getBuildableSpecialWall()
+        {
+            return this.specialWall;
+        }
+
+    }
+}
---
+++ ProjectClient/Class/Builder/SpikesBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectClient.Class.Builder
+{
+    class SpikesBuilder : Builder
+    {
+        public void AddProjectiles()
+        {
+
+        }
+
+        public void AddSlow()
+        {
+
+        }
+    }
+}
