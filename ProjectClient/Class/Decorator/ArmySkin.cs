7d7dd9bd0ac5d990837b8424bf5fbb3606b64344
duokitacc <61093165+duokitacc@users.noreply.github.com>
2021-11-11 07:44:30 +0200

    ggg
    kazkas padaryta minimaliai su decorator

---
+++ ProjectClient/Class/Decorator/ArmySkin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjectClient.Class.Decorator
+{
+    class ArmySkin : SkinDecorator
+    {
+        private PictureBox _display;
+        public override void PutOnSkin()
+        {
+            base.PutOnSkin();
+        }
+    }
+}
---
+++ ProjectClient/Class/Decorator/SkinDecorator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectClient.Class.Decorator
+{
+    public abstract class SkinDecorator
+    {
+        protected readonly Player
+     }
+}
