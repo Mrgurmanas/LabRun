a8eccc8af40cf70eefba44d20ceeaef0ff037963
duokitacc <61093165+duokitacc@users.noreply.github.com>
2021-11-12 11:19:50 +0200

    commando
    fsdf

---
+++ ProjectClient/Class/Command/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace ProjectClient.Class.Command
+{
+    
+    class PlayerController
+    {
+
+        private List<ICommand> list = new List<ICommand>();
+        public List<ICommand> getList()
+        {
+            return list;
+        }
+        public void setList(List<ICommand> list)
+        {
+            this.list = list;
+        }
+        public void run(ICommand cmd)
+        {
+            list.Add(cmd);
+            cmd.execute();
+        }
+        public void undo()
+        {
+            int index = list.Count;
+            ICommand cmd = list[index - 1];
+            list.RemoveAt(index-1);
+
+
+            
+            cmd.undo();
+        }
+        
+    }
+
+    
+}
