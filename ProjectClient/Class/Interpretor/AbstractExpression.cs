d82d15c62ac80165aee2adc6b7c994fe052cdfc6
duokitacc <61093165+duokitacc@users.noreply.github.com>
2021-12-09 21:49:16 +0200

    interpretatoriaus pradzia
    dsddsfds

---
+++ ProjectClient/Class/Interpretor/AbstractExpression.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectClient.Class.Interpretor
+{
+    public abstract class AbstractExpression
+    {
+        public abstract int execute();
+
+        public AbstractExpression Interpret(Context context)
+        {
+            //if(context.Input.Length == 0)
+            //{
+            //    return;
+            //}
+            if(context.Input == "MOVE" || context.Input == "move" || context.Input == "m")
+            {
+                //AbstractExpression exp = new Command()
+            }
+            return null;
+        }
+    }
+}
