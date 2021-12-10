322ee147267cb8e3c560a8dd36a72eb87c5b2126
adomej99 <mejarasadomas@gmail.com>
2021-12-10 03:03:18 +0200

    update

---
+++ ProjectClient/Class/Memento/Caretaker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectClient.Class.Memento
+{
+    public class Caretaker
+    {
+        MementoClass memento;
+        public MementoClass Memento
+        {
+            set { memento = value; }
+            get { return memento; }
+        }
+    }
+}
---
+++ ProjectClient/Class/Memento/MementoClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectClient.Class.Memento
+{
+    public class MementoClass
+    {
+        public string state;
+        public MementoClass(string state)
+        {
+            this.state = state;
+        }
+    }
+}
